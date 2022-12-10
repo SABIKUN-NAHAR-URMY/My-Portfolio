73fb617d404d68e9de7dcae69b9fdf4e67b5627b
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-12-10 23:53:25 +0600

    done all

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -10,9 +10,9 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li className='font-bold text-2xl'><Link>Home</Link></li>
-                        <li className='font-bold text-2xl'><Link>About Me</Link></li>
-                        <li className='font-bold text-2xl'><Link>Projects</Link></li>
+                        <li className='font-bold text-2xl'><Link to={'/'}>Home</Link></li>
+                        {/* <li className='font-bold text-2xl'><Link>About Me</Link></li>
+                        <li className='font-bold text-2xl'><Link>Projects</Link></li> */}
                         <li className='font-bold text-2xl'><Link to={'/contactMe'}>Contact Me</Link>
                         </li>
                     </ul>
@@ -21,9 +21,9 @@ const Header = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li className='font-bold text-xl'><Link>Home</Link></li>
-                    <li className='font-bold text-xl'><Link>About Me</Link></li>
-                    <li className='font-bold text-xl'><Link>Projects</Link></li>
+                    <li className='font-bold text-xl'><Link to={'/'}>Home</Link></li>
+                    {/* <li className='font-bold text-xl'><Link to={'/'}>About Me</Link></li>
+                    <li className='font-bold text-xl'><Link>Projects</Link></li> */}
                     <li className='font-bold text-xl'><Link to={'/contactMe'}>Contact Me</Link></li>
 
                 </ul>
